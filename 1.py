66ba144ade1b5677e5e524e2800549092690cca4
FFFblack <1071910871@qq.com>
2023-05-12 01:29:44 +0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,23 @@
+# import this
+
+import turtle
+
+import sys
+
+print(sys.version_info)
+print(sys.version)
+
+# turtle.pensize(4)
+# turtle.pencolor('red')
+#
+#
+# turtle.right(135)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+#
+# turtle.mainloop()
